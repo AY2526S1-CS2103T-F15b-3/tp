b2bb2fa74b28d405810a12c3d80dee0b6fc04408
soomin-k <oncesoomin0930@gmail.com>
2025-09-24 01:04:17 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -15,12 +15,13 @@ import seedu.address.model.person.Remark;
  * Parses input arguments and creates a new {@code RemarkCommand} object
  */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
+
     /**
      * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
      * and returns a {@code RemarkCommand} object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-     public RemarkCommand parse(String args) throws ParseException {
+    public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
 
@@ -32,10 +33,8 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
                     RemarkCommand.MESSAGE_USAGE), ive);
         }
 
-         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
-
-         return new RemarkCommand(index, new Remark(remark));
-
+        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        return new RemarkCommand(index, new Remark(remark));
     }
 }
 
